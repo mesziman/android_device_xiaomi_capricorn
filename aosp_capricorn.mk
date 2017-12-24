f0dc10e02b28b499f259b9e306be1056628437f9
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2017-12-24 12:25:35 +0000

    v

---
+++ aosp_capricorn.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/xiaomi/capricorn/full_capricorn.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+PRODUCT_NAME := aosp_capricorn
+PRODUCT_DEVICE := capricorn
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 5s
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="capricorn" \
+    PRODUCT_NAME="capricorn" \
+    BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T V8.2.4.0.MAGCNDL release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
